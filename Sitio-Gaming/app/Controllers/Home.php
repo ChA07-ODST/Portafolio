54852ad430473af6332552add726a0c7b0ed813c
ChA07-ODST <christianaguif07@gmail.com>
2026-03-04 21:53:40 -0600

    Arreglo de las rutas

--- Sitio-Gaming/app/Controllers/Home.php
+++ Sitio-Gaming/app/Controllers/Home.php
@@ -4,25 +4,8 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function inicio()
-    {
-        // Aquí podemos pasar datos a la vista si quisiéramos
-        $data = [
-            'titulo' => 'Mi Portafolio con CI4',
-            'mensaje' => 'Bienvenido a mi nuevo proyecto desde cero'
-        ];
-
-        return view('inicio', $data); 
-    }
-
     public function index()
     {
-        // Aquí podemos pasar datos a la vista si quisiéramos
-        $data = [
-            'titulo' => 'Mi Portafolio con CI4',
-            'mensaje' => 'Bienvenido a mi nuevo proyecto desde cero'
-        ];
-
-        return view('welcome_message', $data); 
+        return view('inicio_gaming');
     }
 }
